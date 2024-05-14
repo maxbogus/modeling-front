68b2149655c29f37dc838721b305ca1bbb6d7594
Maxim Boguslavskiy <max.bogus@gmail.com>
2024-05-15 00:07:45 +0300

    refactored 3lab

--- src/Lab3/index.tsx
+++ src/Lab3/index.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable prettier/prettier */
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { postData } from '../utils';
 
 interface TableData {
@@ -57,53 +56,56 @@ const Check = () => {
   );
 };
 
-const showTables = ({algData = [], tableCoeff, tableData = [], algCoeff}: TableData) => <div style={{display: 'flex'}}><div style={{ display: 'flex', flexDirection: 'column', margin: 8 }}>
-<h6>Algorithmic results:</h6>
-<div style={{ maxHeight: '600px', height: '100%', overflowY: 'auto' }}>
-  {algData.map((item, index) => (
-    <div key={index} style={{ display: 'flex', border: '1px solid white' }}>
-      <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{index}</p>
-      <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{item[0]}</p>
-      <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{item[1]}</p>
-      <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{item[2]}</p>
+const showTables = ({ algData = [], tableCoeff, tableData = [], algCoeff }: TableData) => (
+  <div style={{ display: 'flex' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', margin: 8 }}>
+      <h6>Algorithmic results:</h6>
+      <div style={{ maxHeight: '600px', height: '100%', overflowY: 'auto' }}>
+        {algData.map((item, index) => (
+          <div key={index} style={{ display: 'flex', border: '1px solid white' }}>
+            <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{index}</p>
+            <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{item[0]}</p>
+            <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{item[1]}</p>
+            <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{item[2]}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <p>Table coeff:</p>
+        <div style={{ border: '1px solid white', display: 'flex' }}>
+          {tableCoeff?.map((item) => (
+            <p style={{ margin: 0, padding: 8, borderLeft: '1px solid white' }} key={item}>
+              {`${item}`.slice(0, 5)}
+            </p>
+          ))}
+        </div>
+      </div>
     </div>
-  ))}
-</div>
-<div>
-  <p>Table coeff:</p>
-  <div style={{ border: '1px solid white', display: 'flex' }}>
-    {tableCoeff?.map((item) => (
-      <p style={{ margin: 0, padding: 8, borderLeft: '1px solid white' }} key={item}>
-        {`${item}`.slice(0, 5)}
-      </p>
-    ))}
-  </div>
-</div>
-</div>
-<div style={{ display: 'flex', flexDirection: 'column', margin: 8 }}>
-<h6>Table results:</h6>
-<div style={{ maxHeight: '600px', height: '100%', overflowY: 'auto' }}>
-  {tableData.map((item, index) => (
-    <div key={index} style={{ display: 'flex', border: '1px solid white' }}>
-      <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{index}</p>
-      <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{item[0]}</p>
-      <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{item[1]}</p>
-      <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{item[2]}</p>
+    <div style={{ display: 'flex', flexDirection: 'column', margin: 8 }}>
+      <h6>Table results:</h6>
+      <div style={{ maxHeight: '600px', height: '100%', overflowY: 'auto' }}>
+        {tableData.map((item, index) => (
+          <div key={index} style={{ display: 'flex', border: '1px solid white' }}>
+            <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{index}</p>
+            <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{item[0]}</p>
+            <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{item[1]}</p>
+            <p style={{ borderLeft: '1px solid white', padding: 8, margin: 0 }}>{item[2]}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <p>Alg coeff:</p>
+        <div style={{ border: '1px solid white', display: 'flex' }}>
+          {algCoeff?.map((item) => (
+            <p style={{ margin: 0, padding: 8, borderLeft: '1px solid white' }} key={item}>
+              {`${item}`.slice(0, 5)}
+            </p>
+          ))}
+        </div>
+      </div>
     </div>
-  ))}
-</div>
-<div>
-  <p>Alg coeff:</p>
-  <div style={{ border: '1px solid white', display: 'flex' }}>
-    {algCoeff?.map((item) => (
-      <p style={{ margin: 0, padding: 8, borderLeft: '1px solid white' }} key={item}>
-        {`${item}`.slice(0, 5)}
-      </p>
-    ))}
   </div>
-</div>
-</div>
-</div>;
+);
 
 const Tables = () => {
   const [tableData, setTableData] = useState<TableData | undefined>();
@@ -116,13 +118,21 @@ const Tables = () => {
     setIsLoading(false);
   };
 
+  const showTableData = (tableData?: TableData) =>
+    tableData !== undefined ? showTables(tableData) : <></>;
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
       <button style={{ width: 100, margin: 8 }} onClick={() => getTableData()}>
         Load data
       </button>
-      {!isLoading ? tableData !== undefined ? 
-        showTables(tableData) : <></> : <p>Data is loading</p>}
+      {!isLoading ? showTableData(tableData) : <p>Data is loading</p>}
     </div>
   );
 };
